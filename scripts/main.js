9be7e6ece0eea0264c9d5909502c17502ac67a53
Jakob Upton <jakejupton@gmail.com>
2024-03-13 00:12:08 -0700

    added seats and clicking them sets a state

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,13 @@
-function toggleSidebar() {
-  let sidebar = document.querySelector(".sidebar");
-  sidebar.classList.toggle("active");
-}
+// Get all elements with the .seat class
+const seats = document.querySelectorAll('.seat');
+var rootStyles = getComputedStyle(document.documentElement);
+var availableColor = rootStyles.getPropertyValue("--primary-color");
+var selectedColor = rootStyles.getPropertyValue("--accent-color");
+var reservedColor = "white"
+
+// Add a click event listener to each seat
+seats.forEach(seat => {
+  seat.addEventListener('click', () => {
+    seat.setAttribute('selected', 'true')
+  });
+});
