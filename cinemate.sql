0cd9b14efaf788c1643402f094f5f41d64cab27f
Jakob Upton <jakejupton@gmail.com>
2024-03-17 11:24:56 -0700

    updated db dump

--- cinemate.sql
+++ cinemate.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Mar 17, 2024 at 12:36 AM
+-- Generation Time: Mar 17, 2024 at 11:24 AM
 -- Server version: 10.5.21-MariaDB-0+deb11u1
 -- PHP Version: 7.4.33
 
@@ -54,8 +54,10 @@ CREATE TABLE `movies` (
 --
 
 INSERT INTO `movies` (`imdb_id`, `title`, `year`, `cover_path`, `genres`, `description`, `rating`) VALUES
-('tt0091042', 'Ferris Bueller\'s Day Off', '1986', 'https://m.media-amazon.com/images/M/MV5BOWQ1ZDgyYTMtZjEzZS00MmM2LWI1MDItOTM4MTZmZDJmZDExXkEyXkFqcGdeQXVyNDY2MTk1ODk@._V1_FMjpg_UX1059_.jpg', 'comedy', 'A popular high school student, admired by his peers, decides to take a day off from school and goes to extreme lengths to pull it off, to the chagrin of his Dean, who\'ll do anything to stop him.', 7.8),
-('tt15239678', 'Dune: Part Two', '2024', 'https://m.media-amazon.com/images/M/MV5BOWYxNzkwZGYtZDZiNy00MjkzLTkwNTctNzY4YmI0MTNiNGM2XkEyXkFqcGdeQXVyMTEyMjM2NDc2._V1_FMjpg_UY2048_.jpg', 'Action,Adventure,Drama', 'Paul Atreides unites with Chani and the Fremen while on a warpath of revenge against the conspirators who destroyed his family. Facing a choice between the love of his life and the fate of the known universe, he endeavors to prevent a terrible future only he can foresee.', 8.9);
+('tt0091042', 'Ferris Bueller\'s Day Off', '1986', 'https://m.media-amazon.com/images/M/MV5BMDA0NjZhZWUtNmI2NC00MmFjLTgwZDYtYzVjZmNhMDVmOTBkXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_QL75_UX380_CR0,13,380,562_.jpg', 'comedy', 'A popular high school student, admired by his peers, decides to take a day off from school and goes to extreme lengths to pull it off, to the chagrin of his Dean, who\'ll do anything to stop him.', 7.8),
+('tt0816692', 'Interstellar', '2014', 'https://m.media-amazon.com/images/M/MV5BZjdkOTU3MDktN2IxOS00OGEyLWFmMjktY2FiMmZkNWIyODZiXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_FMjpg_UY3600_.jpg', 'adventure,drama,sci-fi', 'In Earth\'s future, a global crop blight and second Dust Bowl are slowly rendering the planet uninhabitable. Professor Brand (Michael Caine), a brilliant NASA physicist, is working on plans to save mankind by transporting Earth\'s population to a new home via a wormhole. But first, Brand must send former NASA pilot Cooper (Matthew McConaughey) and a team of researchers through the wormhole and across the galaxy to find out which of three planets could be mankind\'s new home.', 8.7),
+('tt15239678', 'Dune: Part Two', '2024', 'https://m.media-amazon.com/images/M/MV5BN2QyZGU4ZDctOWMzMy00NTc5LThlOGQtODhmNDI1NmY5YzAwXkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_QL75_UX380_CR0,0,380,562_.jpg', 'Action,Adventure,Drama', 'Paul Atreides unites with Chani and the Fremen while on a warpath of revenge against the conspirators who destroyed his family. Facing a choice between the love of his life and the fate of the known universe, he endeavors to prevent a terrible future only he can foresee.', 8.9),
+('tt4633694', 'Spider-Man: Into the Spider-Verse', '2018', 'https://m.media-amazon.com/images/M/MV5BMjMwNDkxMTgzOF5BMl5BanBnXkFtZTgwNTkwNTQ3NjM@._V1_QL75_UX380_CR0,1,380,562_.jpg', 'animation,action,adventure', 'Bitten by a radioactive spider in the subway, Brooklyn teenager Miles Morales suddenly develops mysterious powers that transform him into the one and only Spider-Man. When he meets Peter Parker, he soon realizes that there are many others who share his special, high-flying talents. Miles must now use his newfound skills to battle the evil Kingpin, a hulking madman who can open portals to other universes and pull different versions of Spider-Man into our world.', 8.4);
 
 -- --------------------------------------------------------
 
@@ -77,7 +79,10 @@ CREATE TABLE `showtimes` (
 INSERT INTO `showtimes` (`show_id`, `imdb_id`, `time`, `date`) VALUES
 (1, 'tt0091042', '10:30pm', 'March 19th, 2024'),
 (2, 'tt0091042', '8:30pm', 'March 20th, 2024'),
-(3, 'tt15239678', '6:30pm', 'March 19th, 2024');
+(3, 'tt15239678', '6:30pm', 'March 19th, 2024'),
+(4, 'tt0816692', '3:30pm', 'March 18th, 2024'),
+(5, 'tt0816692', '7:00pm', 'March 21st, 2024'),
+(6, 'tt4633694', '8:30pm', 'March 20th, 2024');
 
 -- --------------------------------------------------------
 
